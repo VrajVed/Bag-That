6c3f8ef1715c700973dcbd6a2250f97bd1347bad
Vraj Ved <vraj.a.ved@gmail.com>
2025-02-22 16:40:03 +0530

    Updated README

--- frontend-bag/src/App.js
+++ frontend-bag/src/App.js
@@ -3,20 +3,20 @@ import CouponFinder from "./Components/CouponFinder";
 import { useEffect, useState } from "react"; // I added this here to call the backend - Vraj
 
 function App() {                                
-  // const [data, setData] =  useState([{}])   
+   const [data, setData] =  useState([{}])   
 
-  // useEffect(() => {
-  //     fetch("/test").then(  
-  //       res => res.json()
+   useEffect(() => {
+       fetch("/test").then(  
+         res => res.json()
 
-  //     ).then(
-  //       data =>  {
-  //         setData(data)     
-  //         console.log(data)    
+       ).then(
+         data =>  {
+           setData(data)     
+           console.log(data)    
                                
-  //       }
-  //     )
-  //   }, [])
+        }
+       )
+     }, [])
   return (
     <div className="bg-gray-900 min-h-screen p-4">
     <main className="max-w-3xl mx-auto bg-gray-900 rounded-lg shadow-md p-6">
